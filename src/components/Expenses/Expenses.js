1e18f4fc9489b5b4be1f1e1c5c263789fdbf5bc9
Venkatesan R S <wenkatesan@gmail.com>
2023-10-10 12:59:11 +0530

    filter works properly
    subExpenses..
    array.filter()

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -4,36 +4,33 @@ import Card from "../UI/Card";
 import "./Expenses.css";
 import ExpenseFilter from "./ExpensesFilter";
 const Expenses = (props) => {
-  const [filteredYear,setFilteredYear] = useState('2020')
-  const filterChangeHandler = (selectedYear) =>{
-    console.log("inside Expenses.js")
-    console.log(selectedYear)
+  const [filteredYear, setFilteredYear] = useState("2020");
+  const filterChangeHandler = (selectedYear) => {
+    console.log("inside Expenses.js");
+    console.log(selectedYear);
     setFilteredYear(selectedYear);
-  }
+    
+  };
+  //Assignment code
+  //sub expenses array filter
+  const subExpenses = props.expenses.filter((subexp)=>subexp.date.getFullYear().toString()=== filteredYear)
+  console.log(subExpenses)
   return (
+    //also changed props.expenses.map to subExpenses.map
     <div>
       <Card className="expenses">
-      <ExpenseFilter selected = {filteredYear} onChangeFilter = {filterChangeHandler} />
-        <ExpenseItem
-          title={props.expenses[0].title}
-          amount={props.expenses[0].amount}
-          date={props.expenses[0].date}
-        ></ExpenseItem>
-        <ExpenseItem
-          title={props.expenses[1].title}
-          amount={props.expenses[1].amount}
-          date={props.expenses[1].date}
-        ></ExpenseItem>
-        <ExpenseItem
-          title={props.expenses[2].title}
-          amount={props.expenses[2].amount}
-          date={props.expenses[2].date}
-        ></ExpenseItem>
-        <ExpenseItem
-          title={props.expenses[3].title}
-          amount={props.expenses[3].amount}
-          date={props.expenses[3].date}
-        ></ExpenseItem>
+        <ExpenseFilter
+          selected={filteredYear}
+          onChangeFilter={filterChangeHandler}
+        />
+        {subExpenses.map((expense) => (
+          <ExpenseItem
+            key = {expense.id}
+            title={expense.title}
+            amount={expense.amount}
+            date={expense.date}
+          ></ExpenseItem>
+        ))}
       </Card>
     </div>
   );
